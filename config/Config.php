851bbc66067084e9d9a390ff8b6ebde516767f8c
raza11409652 <raza.11409652@lpu.in>
2020-08-07 12:12:37 +0530

    test

--- config/Config.php
+++ config/Config.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 date_default_timezone_set("Asia/Calcutta");
 define('user', 'root');
-define('password' , ''); 
+define('password' , 'root'); 
 define('server' , 'localhost') ; 
 define('dbName'   , 'api');  
 
